7d692a81acb5ce82c2fb242dfe3416e8c00c963b
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-09-22 20:13:56 +0300

    Updates

---
+++ precache-manifest.63f7753af606e2b6f75e965ac2fc9ed3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a78945a1f76874c07f71f6877cf5195a",
+    "url": "/WeRdevs/index.html"
+  },
+  {
+    "revision": "0293d07d72049f18d6c8",
+    "url": "/WeRdevs/static/css/about-us-page.075a8fa4.chunk.css"
+  },
+  {
+    "revision": "c2fdb8cca30a4f55bf31",
+    "url": "/WeRdevs/static/css/home-page.a7844af7.chunk.css"
+  },
+  {
+    "revision": "8f7b8537b4d47e10fd3f",
+    "url": "/WeRdevs/static/css/main.60051539.chunk.css"
+  },
+  {
+    "revision": "7e186ee16e500480f2bb",
+    "url": "/WeRdevs/static/js/4.5c11db7b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/WeRdevs/static/js/4.5c11db7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0293d07d72049f18d6c8",
+    "url": "/WeRdevs/static/js/about-us-page.5d10cc73.chunk.js"
+  },
+  {
+    "revision": "c2fdb8cca30a4f55bf31",
+    "url": "/WeRdevs/static/js/home-page.5e20019b.chunk.js"
+  },
+  {
+    "revision": "8f7b8537b4d47e10fd3f",
+    "url": "/WeRdevs/static/js/main.eedd66ad.chunk.js"
+  },
+  {
+    "revision": "79c4b763fe1004268dcb",
+    "url": "/WeRdevs/static/js/runtime-main.4e384144.js"
+  },
+  {
+    "revision": "1aef4427bd88c216da9951a258423286",
+    "url": "/WeRdevs/static/media/header.1aef4427.jpg"
+  },
+  {
+    "revision": "a38126fc609c6eeb16cc4e1106c77a06",
+    "url": "/WeRdevs/static/media/left-arrow.a38126fc.svg"
+  },
+  {
+    "revision": "34d6dd652fae9cad015cfa7ab1fb54fd",
+    "url": "/WeRdevs/static/media/logo.34d6dd65.svg"
+  },
+  {
+    "revision": "c49c1c3d0caf4d9f25e1eaf1e9bfb1f7",
+    "url": "/WeRdevs/static/media/next.c49c1c3d.svg"
+  }
+]);
